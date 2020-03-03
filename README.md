fbffeca7eece8cef66dad49c500bf9319f616e1f
Eryk Zalejski <ezalejski@gmail.com>
2020-03-03 12:26:26 +0100

    initial_module content merge

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
-# terraform-google-glusterfs
+# terraform-glusterfs
+This module creates Glusterfs cluster and provides endpoints to use with kubernetes. By default cluster is set to three GlusterFS servers, one server per Google Cloud zone in the same chosen region.
+
+![Architecture](images/glusterf-gce-architecture.png)
+
+## Prerequisites
+
+Before continuing, please make sure you have:
+
+* A [Google Cloud](https://cloud.google.com) account
+* The [Google Cloud SDK](https://cloud.google.com/sdk/) installed
+
+## 1. Features
+
+- multi-node glusterfs server is created 
+- possibility to create multiple glusterfs volumes
+- outputs ready to use kubernetes endpoint config in json
+- configurable subnet masks
+- configurable disk sizes and types
+- configure which network tags will be allowed to access
+
+
+## 2. Usage:
+
+define cluster module, the example setup can look like::
+
+```hcl
+module "cluster" {
+  source              = "github.com/erento/terraform-google-glusterfs?ref=1.0.0"
+  server_prefix       = "glusterfs-server"
+  data_disk_prefix    = "glusterfs-brick"
+  subnet_mask         = "10.0.0.0/24"
+  ip_offset           = "${var.ip_offset}"
+  data_disk_size      = "500"
+  network             = "your-exisiting-network"
+  subnetwork          = "subnetwork-name-that-module-will-create-for-you"
+  data_disk_snapshot  = "gluster-snapshot-name"
+  data_disk_type      = "pd-standard (can also be pd-ssd if you need speed)"
+  volume_names        = ["your-volume-name"]
+  tags                = ["gluster", "first_tag", "second_tag"]
+  allowed_source_tags = ["gluster", "first_tag", "second_tag"]
+}
+```
+
+for more settings please look into [variables.tf](variables.tf)
+
+**Important** Please make sure that your main glusterfs tag is defined in allowed_source_tags - this will allow nodes to talk to each other. (by default tags = ["gluster"] and allowed_source_tags = ["gluster"])
+
+### 2.1. Connecting with Kubernetes
+
+1. Apply Kubernetes endpoints and svc files to the cluster:
+```
+kubectl apply -f glusterfs-endpoints.json
+kubectl apply -f files/glusterfs-svc.json
+```
+IMPORTANT note - to make gluster endpoint work persistently in your kubernetes cluster you need to apply glusterfs-endpoints.json (defined in variable kubernetes_endpoint_file_path) and glusterfs-svc.json (located in files folder)
+
+2. Sample mount in deployment:
+```yml
+(...)
+  spec:
+      containers:
+      - image: nginx
+        (...)
+        volumeMounts:
+        - mountPath: /mnt/glusterfs
+          name: glusterfs-vol
+      volumes:
+      - name: glusterfs-vol
+        glusterfs:
+          endpoints: glusterfs-cluster
+          path: volume_name
+          readOnly: false
+```
+
+## 3. Authors
+
+- [Eryk Zalejski](https://github.com/ezalejski)
+
+- [Filip Haftek](https://github.com/filiphaftek)
+
+
+## 4. License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details.
+Copyright (c) 2020 Erento GmbH.
+
+# Acknowledgments
+
+Terraform module is based on [glusterfs-gce repository](https://github.com/rimusz/glusterfs-gce)
---
+++ main.tf
@@ -0,0 +1,132 @@
+provider "google" {
+  project = "${var.project}"
+  region  = "${var.region}"
+  version = "~> 2.20.0"
+}
+
+resource "google_compute_instance" "default" {
+  count        = "${var.cluster_size}"
+  name         = "${var.server_prefix}-${count.index}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.region}-${var.zones[count.index]}"
+
+  can_ip_forward = true
+
+  tags = "${var.tags}"
+
+  boot_disk {
+    device_name = "boot"
+
+    initialize_params {
+      image = "${var.image}"
+      size  = "${var.boot_disk_size}"
+      type  = "${var.boot_disk_type}"
+    }
+  }
+
+  attached_disk = [
+    {
+      device_name = "gluster"
+      source      = "${element(formatlist("%v", google_compute_disk.default.*.name), count.index)}"
+    },
+  ]
+
+  network_interface {
+    subnetwork = "${var.subnetwork}"
+    network_ip = "${cidrhost(var.subnet_mask, -count.index - var.ip_offset)}" # This takes last 3 IPs of the subnet (they are usually free - if not ip_offset can be uset to shift ips)
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata {
+    VmDnsSetting = "${var.vm_dns_setting}"
+  }
+
+  metadata_startup_script = "${element(formatlist("%v", data.template_file.provision_script.*.rendered), count.index)}"
+
+  service_account {
+    scopes = ["compute-rw", "logging-write", "monitoring-write", "storage-full"]
+  }
+
+  depends_on = ["google_compute_disk.default", "google_compute_subnetwork.default"]
+}
+
+resource "google_compute_disk" "default" {
+  count = "${var.cluster_size}"
+  name  = "${var.data_disk_prefix}-${count.index}"
+  type  = "${var.data_disk_type}"
+  zone  = "${var.region}-${var.zones[count.index]}"
+
+  size     = "${var.data_disk_size}"
+  snapshot = "${var.data_disk_snapshot}"
+
+  labels {
+    environment = "glusterfs"
+  }
+}
+
+data "template_file" "provision_script" {
+  count    = "${var.cluster_size}"
+  template = "${file("${path.module}/files/glusterfs_provision_server.sh")}"
+
+  vars {
+    cluster_size    = "${var.cluster_size}"
+    server_prefix   = "${var.server_prefix}"
+    volume_names    = "${join(" ", var.volume_names)}"
+    replicas_number = "${var.replicas_number}"
+  }
+}
+
+data "template_file" "endpoint" {
+  count    = "${var.cluster_size}"
+  template = "${file("${path.module}/files/endpoint.json.tpl")}"
+
+  vars {
+    ip = "${element(google_compute_instance.default.*.network_interface.0.address, count.index)}"
+  }
+}
+
+data "template_file" "kubernetes_endpoints" {
+  template = "${file("${path.module}/files/kubernetes_endpoints.json.tpl")}"
+
+  vars {
+    endpoints                = "${join(",\n    ", data.template_file.endpoint.*.rendered)}"
+    kubernetes_endpoint_name = "${var.kubernetes_endpoint_name}"
+  }
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "${var.subnetwork}"
+  ip_cidr_range = "${var.subnet_mask}"
+  region        = "${var.region}"
+  network       = "${var.network}"
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "gluster-firewall"
+  network = "${var.network}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["1-65535"]
+  }
+
+  source_tags = "${var.allowed_source_tags}"
+  target_tags = "${var.tags}"
+}
+
+resource "null_resource" "export_rendered_template" {
+  provisioner "local-exec" {
+    command = "cat > ${var.kubernetes_endpoint_file_path} <<EOL\n${data.template_file.kubernetes_endpoints.rendered}\nEOL"
+  }
+
+  triggers = {
+    update_config = "${timestamp()}"
+  }
+}
